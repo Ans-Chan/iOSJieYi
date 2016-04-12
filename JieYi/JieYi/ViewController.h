6a69ef59a35d646763196b6609c1d922a6290c31
wangchan <153650401@qq.com>
2016-04-12 20:27:52 +0800

    项目初始化

---
+++ JieYi/JieYi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JieYi
+//
+//  Created by chan on 16/4/12.
+//  Copyright © 2016年 chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JieYi/JieYi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JieYi
+//
+//  Created by chan on 16/4/12.
+//  Copyright © 2016年 chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
